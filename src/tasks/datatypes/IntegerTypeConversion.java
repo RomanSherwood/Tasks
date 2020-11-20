814dbfff1e56c9c4379cec9db6444b083b9e8d20
romansherwood <sherwoodroma777@gmail.com>
2020-11-20 20:48:34 +0300

    3. Приведение типов. Целые числа

---
+++ src/tasks/datatypes/IntegerTypeConversion.java
@@ -0,0 +1,11 @@
+package tasks.datatypes;
+
+public class IntegerTypeConversion {
+    public static void main(String[] args) {
+        int num = 1;
+        long numUpper = num;
+        short numLower = (short) num;
+        System.out.println(numUpper);
+        System.out.println(numLower);
+    }
+}
