de662d6e2b7e9b9b49e4b1e14197969f0f483b68
romansherwood <sherwoodroma777@gmail.com>
2020-11-20 20:43:55 +0300

    1. int число

---
+++ src/tasks/datatypes/IntType.java
@@ -0,0 +1,11 @@
+package tasks.datatypes;
+
+import java.util.Scanner;
+
+public class IntType {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int value = in.nextInt();
+        System.out.println(value);
+    }
+}
