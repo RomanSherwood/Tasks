bd76f30763541acca9a604f5f0d5ad6d123aeb20
romansherwood <sherwoodroma777@gmail.com>
2020-11-20 20:52:12 +0300

    5. Целая часть вещественного числа

---
+++ src/tasks/datatypes/IntegerPart.java
@@ -0,0 +1,9 @@
+package tasks.datatypes;
+
+public class IntegerPart {
+    public static void main(String[] args) {
+        float floatNum = 2.6f;
+        int integerNum = (int) floatNum;
+        System.out.println(integerNum);
+    }
+}
