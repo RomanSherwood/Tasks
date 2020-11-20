f68eedfc11d9059434e13a699f077acbb7c7d6dc
romansherwood <sherwoodroma777@gmail.com>
2020-11-20 20:56:01 +0300

    6. Возведения числа в степень

---
+++ src/tasks/datatypes/MathPow.java
@@ -0,0 +1,8 @@
+package tasks.datatypes;
+
+public class MathPow {
+    public static void main(String[] args) {
+        double result = Math.pow(3, 2);
+        System.out.println(result);
+    }
+}
