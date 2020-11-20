ab36071bbcdaccde78c1d7e51e20831235e0c617
romansherwood <sherwoodroma777@gmail.com>
2020-11-20 20:59:27 +0300

    7. Получение корня из числа

---
+++ src/tasks/datatypes/MathSqrt.java
@@ -0,0 +1,8 @@
+package tasks.datatypes;
+
+public class MathSqrt {
+    public static void main(String[] args) {
+        double result = Math.sqrt(9);
+        System.out.println(result);
+    }
+}
