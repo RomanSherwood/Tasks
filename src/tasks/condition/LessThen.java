0ab55c313028356a4dddf5356717717357514cfe
romansherwood <sherwoodroma777@gmail.com>
2020-11-23 14:27:23 +0300

    1.2. Первое меньше второго

---
+++ src/tasks/condition/LessThen.java
@@ -0,0 +1,7 @@
+package tasks.condition;
+
+public class LessThen {
+    public static boolean check(int first, int second) {
+        return first < second;
+    }
+}
---
+++ test/tasks/condition/LessThenTest.java
@@ -0,0 +1,19 @@
+package tasks.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class LessThenTest {
+
+    @Test
+    public void whenFirstLess() {
+        assertThat(LessThen.check(1, 2), is(true));
+    }
+
+    @Test
+    public void whenFirstGreat() {
+        assertThat(LessThen.check(10, 2), is(false));
+    }
+}
